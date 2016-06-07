01693d426fe5aea0f28b9153ccf89c509ba003cb
Enrico Kaack <e.kaack@live.de>
2016-06-07 10:41:42 +0200

    ADDED: Curl and JSON to XML converter, BUG: malformed XML

--- util.php
+++ util.php
@@ -8,17 +8,38 @@ class Utils{
 public function getConnection($url){
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 	$result = curl_exec($ch);
 	curl_close($ch);
-return $result;
+	$this->convertJsonToXml($result);
 }
 
-private function convertJsonToXml($json){
-	return $xml;
+public function convertJsonToXml($json){
+	
+	$array = json_decode ($json, true);
+	$xml = new SimpleXMLElement("<root></root>");
+	$this->arrayToXml($array, $xml);
+	echo $xml->asXML();
 }
 
+public function arrayToXml($array, &$xml){
+   foreach($array as $key => $value) {
+        if(is_array($value)) {
+            if(!is_numeric($key)){
+                $subnode = $xml->addChild("$key");
+                $this->arrayToXml($value, $subnode);
+            }else{
+                $subnode = $xml->addChild("element$key");
+                $this->arrayToXml($value, $subnode);
+            }
+        }else {
+            $xml->addChild("$key",htmlspecialchars("$value"));
+        }
+    }
+}	
 }
 
-//$utils = new Utils();
-//echo($utils->getConnection("http://opendata.dbbahnpark.info/api/beta/stations"));
+$utils = new Utils();
+$utils->getConnection("http://opendata.dbbahnpark.info/api/beta/stations");
+
 ?>
