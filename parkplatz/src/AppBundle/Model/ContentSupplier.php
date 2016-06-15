b903439839debb0e054ab3117caa30d07720cb03
Enrico Kaack <e.kaack@live.de>
2016-06-15 12:59:46 +0200

    ADDED curl parameters

--- parkplatz/src/AppBundle/Model/ContentSupplier.php
+++ parkplatz/src/AppBundle/Model/ContentSupplier.php
@@ -15,4 +15,26 @@ class ContentSupplier {
         $dbmanager = 0;
     }
     
+	private loadGeoCoordinates(){
+		//Load the list of all stations in table stations
+		
+		//delete elements containing geocoordinates
+		
+		//Loop throw all remaining elements
+		
+		//Call to google geocoding API
+		//ATENTION: at the most of 10 requests per second
+		//API-KEY: AIzaSyCqaY1zPxlWWJTZQ1eEWDB4QYOR2jBZL04
+		$ch = curl_init("http://maps.googleapis.com/maps/api/geocode/json");
+		$data = array (
+        'key' => 'AIzaSyCqaY1zPxlWWJTZQ1eEWDB4QYOR2jBZL04',
+		'address ' => '' 
+        );
+        
+        $params = '';
+		foreach($data as $key=>$value)
+                $params .= $key.'='.$value.'&';
+         
+        $params = trim($params, '&');
+	}
 }
