824f611ddfef304b5ec537ec8e807e592949f2a1
Enrico Kaack <e.kaack@live.de>
2016-06-15 15:23:15 +0200

    ADDED Import from CSV for train stations, not tested, doesn't write to db till now

--- parkplatz/src/AppBundle/Model/ContentSupplier.php
+++ parkplatz/src/AppBundle/Model/ContentSupplier.php
@@ -22,21 +22,48 @@ class ContentSupplier {
         $query = $entityMgr->createQuery($statement);
         $date = $query->getResults();
     }
-    
-	private loadGeoCoordinates(){
-		//Load the list of all stations in table stations
+	
+	private function loadStationList(){
+		$list = array();
+			
+		$ch = curl_init("http://data.deutschebahn.com/datasets/haltestellen/D_Bahnhof_2016_01_alle.csv");
+		$result = curl_exec($ch
 		
-		//delete elements containing geocoordinates
+		$rows = explode("\n", $result);
+		foreach($rows as $item){
+			$cell = explode(";", $item);
+			$entity = new MasterStation();
+			$entity->setBahnhofsNummer($cell[0]);
+			$entity->setName($cell[2]);
+			$entity->setLongitude($cell[4]);
+			$entity->setLatitude($cell[5]);
+			$list[] = $entity;
+		}
+		
+		//TODO: write on DB after succesfull testing
+	}
+    
+	//Deprecated
+	private function loadGeoCoordinates(){
+		//Load the list of all stations in table stations
+		$repository = $entityMgr->getRepository('AppBundle:station');
 		
 		//Loop throw all remaining elements
-		
-		//Call to google geocoding API
-		//ATENTION: at the most of 10 requests per second
-		//API-KEY: AIzaSyCqaY1zPxlWWJTZQ1eEWDB4QYOR2jBZL04
-		$ch = curl_init("http://maps.googleapis.com/maps/api/geocode/json");
-		$data = array (
+		foreach ($repository as $element){
+			//Call to google geocoding API
+			//ATENTION: at the most of 10 requests per second
+			//API-KEY: AIzaSyCqaY1zPxlWWJTZQ1eEWDB4QYOR2jBZL04
+			$ch = curl_init();
+			generateCurl($ch, $element->getStreet().','.$element->getPlz().' '.$element->getCityTitle());
+			$result = curl_exec($ch);
+		}
+
+	}
+	
+	private function generateCurl($ch, $adress){
+	$data = array (
         'key' => 'AIzaSyCqaY1zPxlWWJTZQ1eEWDB4QYOR2jBZL04',
-		'address ' => '' 
+		'address' => $adress 
         );
         
         $params = '';
@@ -44,5 +71,8 @@ class ContentSupplier {
                 $params .= $key.'='.$value.'&';
          
         $params = trim($params, '&');
+		
+		curl_setopt($ch, CURLOPT_URL, "http://maps.googleapis.com/maps/api/geocode/json".'?'.$params );
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 	}
 }
