e7a43651548d473825e691bf0b443e385c9a0228
Sebastian Gellweiler <sg@gellweiler.net>
2016-06-17 11:49:31 +0200

    Replaced paths in script.js with relative ones.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   // Load options for stations.
   new Transformation()
-  .setXml("/parkplatz/web/app_dev.php/dblot")
-  .setXslt("/XSLT_Stations.xsl")
+  .setXml("parkplatz/web/app_dev.php/dblot")
+  .setXslt("XSLT_Stations.xsl")
   .transform("station");
 });
 
